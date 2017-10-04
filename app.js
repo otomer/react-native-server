5e08563df4946dd173b6fe28a596e0346b01c366
Oriyan <o.tomerr@gmail.com>
2017-10-05 00:15:27 +0300

    A

--- app.js
+++ app.js
@@ -5,6 +5,84 @@ var mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
 var dateFormat = require('dateformat');
 
- app.listen(port);
+
+app.use(bodyParser.json());
+
+// http://johnzhang.io/options-request-in-express
+
+Msg = require('./models/msgs')
+
+// Your own super cool function
+var logger = function (req, res, next) {
+    console.log("GOT REQUEST !");
+    next(); // Passing the request to the next handler in the stack.
+}
+
+//Middleware
+var requestMiddleWare = function (req, res, next) {
+    var d = dateFormat(Date.now(), "dd/mm/yyyy H:MM:ss TT");
+    console.log(d + ": " + req.method + " " + req.url);
+    next();
+}
+
+app.use(requestMiddleWare)
+
+// Connect to mongoose
+mongoose.connect('mongodb://admin:admin@ds044689.mlab.com:44689/appdb', {
+    useMongoClient: true
+});
+
+app.get('/', function (req, res) {
+    var serverResponse = {
+        "node express version": require('express/package').version,
+        "listening on port": port
+    };
+    res.send(serverResponse);
+})
+
+app.get('/api/msgs', function (req, res) {
+    Msg.getMsgs(function (err, msgs) {
+        if (err) {
+            throw err;
+        } else {
+            res.json(msgs);
+        }
+    })
+})
+
+app.post('/api/msgs', function (req, res) {
+    var msg = req.body;
+    Msg.addMsg(msg, function (err, msg) {
+        if (err) {
+            throw err;
+        } else {
+            res.json(msg);
+        }
+    })
+})
+
+app.put('/api/msgs/:_id', function (req, res) {
+    var id = req.params._id;
+    var msg = req.body;
+    Msg.updateMsg(id, msg, {}, function (err, msg) {
+        if (err) {
+            throw err;
+        } else {
+            res.json(msg);
+        }
+    })
+})
+
+app.get('/api/msgs/:_id', function (req, res) {
+    Msg.getMsgById(req.params._id, function (err, msg) {
+        if (err) {
+            throw err;
+        } else {
+            res.json(msg);
+        }
+    })
+})
+
+app.listen(port);
 
 console.log('Server is listening on port: ' + port)
