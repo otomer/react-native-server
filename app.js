d73931701c3c974c92f34051a9230a62077832b1
Oriyan <o.tomerr@gmail.com>
2017-10-05 00:27:49 +0300

    A

--- app.js
+++ app.js
@@ -1,87 +1,87 @@
 var express = require('express');
 var app = express();
-// var bodyParser = require('body-parser');
-// var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
 var dateFormat = require('dateformat');
 
 
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
-// // http://johnzhang.io/options-request-in-express
+// http://johnzhang.io/options-request-in-express
 
-// Msg = require('./models/msgs')
+Msg = require('./models/msgs')
 
-// // Your own super cool function
-// var logger = function (req, res, next) {
-//     console.log("GOT REQUEST !");
-//     next(); // Passing the request to the next handler in the stack.
-// }
+// Your own super cool function
+var logger = function (req, res, next) {
+    console.log("GOT REQUEST !");
+    next(); // Passing the request to the next handler in the stack.
+}
 
-// //Middleware
-// var requestMiddleWare = function (req, res, next) {
-//     var d = dateFormat(Date.now(), "dd/mm/yyyy H:MM:ss TT");
-//     console.log(d + ": " + req.method + " " + req.url);
-//     next();
-// }
+//Middleware
+var requestMiddleWare = function (req, res, next) {
+    var d = dateFormat(Date.now(), "dd/mm/yyyy H:MM:ss TT");
+    console.log(d + ": " + req.method + " " + req.url);
+    next();
+}
 
-// app.use(requestMiddleWare)
+app.use(requestMiddleWare)
 
-// // Connect to mongoose
-// mongoose.connect('mongodb://admin:admin@ds044689.mlab.com:44689/appdb', {
-//     useMongoClient: true
-// });
+// Connect to mongoose
+mongoose.connect('mongodb://admin:admin@ds044689.mlab.com:44689/appdb', {
+    useMongoClient: true
+});
 
-// app.get('/', function (req, res) {
-//     var serverResponse = {
-//         "node express version": require('express/package').version,
-//         "listening on port": port
-//     };
-//     res.send(serverResponse);
-// })
+app.get('/', function (req, res) {
+    var serverResponse = {
+        "node express version": require('express/package').version,
+        "listening on port": port
+    };
+    res.send(serverResponse);
+})
 
-// app.get('/api/msgs', function (req, res) {
-//     Msg.getMsgs(function (err, msgs) {
-//         if (err) {
-//             throw err;
-//         } else {
-//             res.json(msgs);
-//         }
-//     })
-// })
+app.get('/api/msgs', function (req, res) {
+    Msg.getMsgs(function (err, msgs) {
+        if (err) {
+            throw err;
+        } else {
+            res.json(msgs);
+        }
+    })
+})
 
-// app.post('/api/msgs', function (req, res) {
-//     var msg = req.body;
-//     Msg.addMsg(msg, function (err, msg) {
-//         if (err) {
-//             throw err;
-//         } else {
-//             res.json(msg);
-//         }
-//     })
-// })
+app.post('/api/msgs', function (req, res) {
+    var msg = req.body;
+    Msg.addMsg(msg, function (err, msg) {
+        if (err) {
+            throw err;
+        } else {
+            res.json(msg);
+        }
+    })
+})
 
-// app.put('/api/msgs/:_id', function (req, res) {
-//     var id = req.params._id;
-//     var msg = req.body;
-//     Msg.updateMsg(id, msg, {}, function (err, msg) {
-//         if (err) {
-//             throw err;
-//         } else {
-//             res.json(msg);
-//         }
-//     })
-// })
+app.put('/api/msgs/:_id', function (req, res) {
+    var id = req.params._id;
+    var msg = req.body;
+    Msg.updateMsg(id, msg, {}, function (err, msg) {
+        if (err) {
+            throw err;
+        } else {
+            res.json(msg);
+        }
+    })
+})
 
-// app.get('/api/msgs/:_id', function (req, res) {
-//     Msg.getMsgById(req.params._id, function (err, msg) {
-//         if (err) {
-//             throw err;
-//         } else {
-//             res.json(msg);
-//         }
-//     })
-// })
+app.get('/api/msgs/:_id', function (req, res) {
+    Msg.getMsgById(req.params._id, function (err, msg) {
+        if (err) {
+            throw err;
+        } else {
+            res.json(msg);
+        }
+    })
+})
 
 app.listen(port);
 
