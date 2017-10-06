6015e2dcae5a03d50d9ccdfc668e5f35cabdf27a
Oriyan <o.tomerr@gmail.com>
2017-10-06 22:58:37 +0300

    nodemodulesfix

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //Middleware
 app.use(function (req, res, next) {
     console.log("\r");
-    // var d = dateFormat(Date.now(), "dd/mm/yyyy H:MM:ss TT");
-    // console.log(d + ": " + req.method + " " + req.url);
+    var d = dateFormat(Date.now(), "dd/mm/yyyy H:MM:ss TT");
+    console.log(d + ": " + req.method + " " + req.url);
     var interceptObj = function (obj, key) { if (obj && Object.keys(obj).length > 0) { console.log(key, obj); } }
     interceptObj(req.query, "query");
     interceptObj(req.params, "params");
@@ -36,10 +36,10 @@ app.use(function (req, res, next) {
     next();  // Passing the request to the next handler in the stack.
 });
 
-// // include routes
-// app.use('/auth', require('./routes/authRouter'));
-// app.use('/api', require('./routes/apiRouter'));
-// app.use('/admin', require('./routes/adminRouter'));
+// include routes
+app.use('/auth', require('./routes/authRouter'));
+app.use('/api', require('./routes/apiRouter'));
+app.use('/admin', require('./routes/adminRouter'));
 
 // serve static files from template
 app.use(express.static(__dirname + '/public'));
@@ -47,12 +47,11 @@ app.use(express.static(__dirname + '/public'));
 var serverInfo = {
     expressVersion: require('express/package').version,
     port: port,
-    //upTime: dateFormat(Date.now(), "dd/mm/yyyy H:MM:ss TT"),
+    upTime: dateFormat(Date.now(), "dd/mm/yyyy H:MM:ss TT"),
     authTester: "Navigate to /auth/"
 }
 
 app.get('/', function (req, res) {
-    serverInfo.lasterr = lasterr;
     res.send(serverInfo);
 })
 
